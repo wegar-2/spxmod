3f346e578da50d781dd1b9ab77ddd6952a4c0c3c
wegar2 <awegrzyn17@gmail.com>
2026-07-04 14:13:48 +0200

    completed trend features transformer

--- src/transformer/features/trend_features_transformer.py
+++ src/transformer/features/trend_features_transformer.py
@@ -1,4 +1,6 @@
+from itertools import product
 import logging
+from typing import Final, TypeAlias
 
 import pandas as pd
 
@@ -8,16 +10,20 @@ logger = logging.getLogger(__name__)
 
 __all__ = ["TrendFeaturesTransformer"]
 
+IntTuple: TypeAlias = tuple[int, ...]
+
 
 class TrendFeaturesTransformer:
 
     def __init__(
             self,
-            ma_windows: tuple[int, ...] = (20, 50, 100, 200),
-            zscore_half_lives: tuple[int, ...] = (20, 60, 250)
+            ma_windows: IntTuple = (20, 50, 100, 200),
+            zscore_half_lives: IntTuple = (20, 60, 250),
+            ma_slope_lags: IntTuple = (5, 10, 20)
     ):
-        self._ma_windows: tuple[int, ...] = ma_windows
-        self._zscore_half_lives: tuple[int, ...] = zscore_half_lives
+        self._ma_windows: Final[IntTuple] = ma_windows
+        self._zscore_half_lives: Final[IntTuple] = zscore_half_lives
+        self._ma_slope_lags: Final[IntTuple] = ma_slope_lags
 
     def transform(self, data: pd.DataFrame) -> pd.DataFrame:
 
@@ -43,7 +49,27 @@ class TrendFeaturesTransformer:
                 )
 
         logging.info("Calculating MA slopes")
+        for ma_wind, ma_slope_lag in product(
+                self._ma_windows,
+                self._ma_slope_lags
+        ):
+            logger.info(f"MA {ma_wind}D slope change over {ma_slope_lag}D")
+            data[f"ma_{ma_wind}D_slope_growth_{ma_slope_lag}D"] = (
+                data[f"close_ma_{ma_wind}D"] /
+                data[f"close_ma_{ma_wind}D"].shift(ma_slope_lag)
+            ) - 1
 
-
+        logger.info("Classic crossovers between MAs and/or close price")
+        data["MA20_gt_MA50"] = (
+            data["close"].rolling(window=20).mean() >
+            data["close"].rolling(window=50).mean()
+        ).astype(int)
+        data["MA50_gt_MA200"] = (
+            data["close"].rolling(window=50).mean() >
+            data["close"].rolling(window=200).mean()
+        ).astype(int)
+        data["close_above_MA200"] = (
+                data["close"] > data["close"].rolling(window=200).mean()
+        ).astype(int)
 
         return data
