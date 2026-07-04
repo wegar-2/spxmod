28890831c6301584ab7971ae15aaf3f6a28c8f2c
wegar2 <awegrzyn17@gmail.com>
2026-07-04 21:13:54 +0200

    completed volume features transformer

--- src/transformer/features/volume_features_transformer.py
+++ src/transformer/features/volume_features_transformer.py
@@ -15,9 +15,13 @@ class VolumeFeaturesTransformer:
 
     def __init__(
             self,
-            volume_half_lives: IntTuple = (20, 60, 250)
+            volume_half_lives: IntTuple = (20, 60, 250),
+            obv_lag: int = 20,
+            obv_change_half_lives: IntTuple = (20, 60, 250)
     ):
         self._volume_half_lives: IntTuple = volume_half_lives
+        self._obv_lag: int = obv_lag
+        self._obv_change_half_lives: IntTuple = obv_change_half_lives
 
     def transform(self, data: pd.DataFrame) -> pd.DataFrame:
 
@@ -45,7 +49,19 @@ class VolumeFeaturesTransformer:
                 ewm_zscore(data=data["log_volume"], hl=hl))
 
 
-        logger.info("VOLUME 2/2. On-balance volume")
+        logger.info("VOLUME 2/2. On-balance volume (OBV)")
 
+        logger.info("On-balance volume (OBV)")
+        signed_volume = np.sign(data["log_return"]) * data["volume"]
+        data["obv"] = signed_volume.cumsum()
+
+        logger.info("Change of on-balance volume")
+        data[f"obv_change_{self._obv_lag}D"] = (
+            data["obv"] / data["obv"].shift(self._obv_lag)) - 1
+
+        logger.info("Calculating zscores of obv changes")
+        for hl in self._obv_change_half_lives:
+            data[f"obv_change_{self._obv_lag}D_zscore_hl{hl}"] = ewm_zscore(
+                data=data[f"obv_change_{self._obv_lag}D"], hl=hl)
 
         return data
